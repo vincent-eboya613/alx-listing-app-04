d682d2e567d64ad508898dbc39c813247d196a5e
vincent-eboya613 <vincentkidavi@gmail.com>
2025-09-02 17:12:36 +0300

    Update and rename [Id].tsx to [id].tsx

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+ import { useRouter } from "next/router";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import PropertyDetail from "@/components/property/PropertyDetail";
